606de65c0fbfc23827504606a5ffcff5e0aa6246
halil-ibrahim-baykan <halilibrahimbaykan@gmail.com>
2019-12-11 20:14:40 +0100

    Initial Commit

---
+++ hotdog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hotdog
+//
+//  Created by baykan on 11/12/2019.
+//  Copyright © 2019 baykan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
